584435db5eecaa829ab905f5f4b3f6cf143d64b6
mpbp97440-byte <mpbp97440@gmail.com>
2026-05-17 21:02:17 +0200

    Add files via upload
    Ajout du site Sparetdee Simon

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>{e.waitUntil(caches.open("sparetdee-v2").then(c=>c.addAll(["./","index.html","style.css","script.js","data.json"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
